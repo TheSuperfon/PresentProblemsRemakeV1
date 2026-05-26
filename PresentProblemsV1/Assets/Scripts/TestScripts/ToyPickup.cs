9c06ba067978d173d88a28f6bb13cf9b268d1ebc
TheSuperfon <astefanadon@gmail.com>
2026-05-26 12:20:54 -0400

    ToyProgress

--- PresentProblemsV1/Assets/Scripts/TestScripts/ToyPickup.cs
+++ PresentProblemsV1/Assets/Scripts/TestScripts/ToyPickup.cs
@@ -2,10 +2,24 @@ using UnityEngine;
 
 public class ToyPickup : MonoBehaviour
 {
+
+    public PauseAttempt1 InventoryReference;
+    public GameObject dinoOBJ;
+    public GameObject blocksOBJ;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        if (SceneChecks.DinoChoice)
+        {
+            blocksOBJ.SetActive(false);
+            dinoOBJ.SetActive(true);
+        }
+        else
+        {
+            blocksOBJ.SetActive(true);
+            dinoOBJ.SetActive(false);
+        }
     }
 
     // Update is called once per frame
@@ -13,4 +27,26 @@ public class ToyPickup : MonoBehaviour
     {
         
     }
+
+    private void OnMouseOver()
+    {
+        
+        if (Input.GetMouseButtonDown(0))
+        {
+            if (SceneChecks.DinoChoice)
+            {
+                InventoryReference.activateDino();
+            }
+            else
+            {
+                InventoryReference.activateBlocks();
+            }
+
+
+
+        }
+
+
+    }
+
 }
